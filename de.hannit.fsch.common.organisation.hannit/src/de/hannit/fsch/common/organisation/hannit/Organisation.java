f67022ed1a6cf215a074e2c64cb6d81e3429e4f5
frankschaare <frank.schaare@gmail.com>
2014-03-18 13:01:22 +0100

    20140318: NavTree erweitert

--- de.hannit.fsch.common.organisation.hannit/src/de/hannit/fsch/common/organisation/hannit/Organisation.java
+++ de.hannit.fsch.common.organisation.hannit/src/de/hannit/fsch/common/organisation/hannit/Organisation.java
@@ -6,6 +6,8 @@ package de.hannit.fsch.common.organisation.hannit;
 import java.util.Date;
 import java.util.TreeMap;
 
+import de.hannit.fsch.common.mitarbeiter.Mitarbeiter;
+import de.hannit.fsch.common.mitarbeiter.Team;
 import de.hannit.fsch.common.organisation.IOrganisation;
 import de.hannit.fsch.common.organisation.reporting.Monatsbericht;
 
@@ -17,6 +19,10 @@ public class Organisation implements IOrganisation
 {
 private final String name = "Hannoversche Informationstechnologien";
 private TreeMap<Date, Monatsbericht> monatsBerichte = new TreeMap<Date, Monatsbericht>(); 
+
+private TreeMap<Integer, Mitarbeiter> mitarbeiterPNR = null;
+private TreeMap<String, Mitarbeiter> mitarbeiterNachname = null;
+private TreeMap<Integer, Team> teams = null;
 
 	/**
 	 * 
@@ -31,4 +37,36 @@ private TreeMap<Date, Monatsbericht> monatsBerichte = new TreeMap<Date, Monatsbe
 	 */
 	@Override
 	public String getName() {return this.name;}
+
+	@Override
+	public void setMitarbeiter(TreeMap<Integer, Mitarbeiter> incoming)
+	{
+	this.mitarbeiterPNR = incoming;	
+	mitarbeiterNachname = new TreeMap<String, Mitarbeiter>();	
+	teams = new TreeMap<Integer, Team>();
+	
+		for (Mitarbeiter mPNR : mitarbeiterPNR.values())
+		{
+		mitarbeiterNachname.put(mPNR.getNachname(), mPNR);
+			
+		int teamNR = mPNR.getTeamNR();
+			if (! teams.containsKey(teamNR))
+			{
+			Team team = new Team(teamNR);
+			teams.put(teamNR, team);
+			}
+		teams.get(teamNR).addMitarbeiter(mPNR);
+		}
+	}
+
+	@Override
+	public TreeMap<String, Mitarbeiter> getMitarbeiterNachName(){return this.mitarbeiterNachname;}
+	@Override
+	public TreeMap<Integer, Mitarbeiter> getMitarbeiterNachPNR() {return this.mitarbeiterPNR;}
+
+	@Override
+	public TreeMap<Integer, Team> getTeams()
+	{
+	return teams;
+	}
 }
